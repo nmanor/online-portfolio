3e3746943a96b1dfc431dc94d3df5f3f202a5895
Natan Manor <natanmanor@gmail.com>
2023-05-04 15:31:20 +0300

    Send application to MongoDB

--- pages/api/send-application.js
+++ pages/api/send-application.js
@@ -2,14 +2,19 @@ import { HttpStatusCode } from 'axios';
 import saveApplication from '../../DAL/mongoose';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(HttpStatusCode.MethodNotAllowed).send({ message: 'Only POST requests allowed' });
-  }
+  try {
+    if (req.method !== 'POST') {
+      return res.status(HttpStatusCode.MethodNotAllowed).send({ message: 'Only POST requests allowed' });
+    }
 
-  const saved = await saveApplication(req.body);
+    const saved = await saveApplication(req.body);
 
-  if (saved) {
-    return res.send({ message: 'Application saved' });
+    if (saved) {
+      return res.send({ message: 'Application saved' });
+    }
+    return res.status(HttpStatusCode.InternalServerError).send({ message: 'An error accord' });
+  } catch (e) {
+    console.error(e);
+    return res.status(HttpStatusCode.InternalServerError).send({ message: 'An error accord' });
   }
-  return res.status(HttpStatusCode.InternalServerError).send({ message: 'An error accord' });
 }
