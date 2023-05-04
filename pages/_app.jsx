67a30d9c6b82d1a1701e4fa7a95e3672713ec8b4
Natan Manor <natanmanor@gmail.com>
2023-05-04 13:12:20 +0300

    Add footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,12 +5,14 @@ import '../styles/globals.css';
 import PropTypes from 'prop-types';
 import { Toaster } from 'react-hot-toast';
 import HeaderComponent from '../components/HeaderComponent/HeaderComponent';
+import FooterComponent from '../components/FooterComponent/FooterComponent';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <HeaderComponent />
       <Component {...pageProps} />
+      <FooterComponent />
       <Toaster position="bottom-center" />
     </>
   );
